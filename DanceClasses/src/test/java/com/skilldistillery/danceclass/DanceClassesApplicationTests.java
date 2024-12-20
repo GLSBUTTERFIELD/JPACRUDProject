02c9eddee81c0a29c66cbeba1e37acde7f80e216
Gabby <glsbutterfield@gmail.com>
2024-12-20 16:07:17 -0700

    create Boot MVC project, link JPA project dependency

---
+++ DanceClasses/src/test/java/com/skilldistillery/danceclass/DanceClassesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.danceclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanceClassesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
